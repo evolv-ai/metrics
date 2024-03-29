38cc7b92bdd5856c72e4b3f25f695e1de89bf164
rcowin <richard.cowin@gmail.com>
2024-03-29 11:37:14 -0500

    add a couple more tests

--- test/spa.test.js
+++ test/spa.test.js
@@ -2,7 +2,23 @@
 import { instrumentSpaEvent } from "../src/spa";
 
 
-test('no return', () => {
-  expect(instrumentSpaEvent('test')).toBe(undefined);
+test('spa event occurs on pushState', () => {
+  let spaFnc = jest.fn(x=>x);
+  instrumentSpaEvent('test');
+
+  window.addEventListener('test', spaFnc)
+  window.history.pushState('/', 'comment');
+
+  expect(spaFnc).toBeCalled();
 });
 
+
+test('spa event occurs on replaceState', () => {
+  let spaFnc = jest.fn(x=>x);
+  instrumentSpaEvent('test');
+
+  window.addEventListener('test', spaFnc)
+  window.history.replaceState('/', 'comment');
+
+  expect(spaFnc).toBeCalled();
+});
