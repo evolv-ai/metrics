255ca607886dff69ab4fe57b64b91ed99c86a6b4
rcowin <richard.cowin@gmail.com>
2024-04-02 14:54:30 -0500

    remove no longer used attribute

--- src/inheritance.js
+++ src/inheritance.js
@@ -5,12 +5,8 @@ const clearsKey = [
 ]
 
 export function mergeMetric(context, metric){
-    let {key, apply, eval_now, value, comment, when, ...baseContext} = context;
+    let {key, apply, value, comment, when, ...baseContext} = context;
 
-    if (eval_now){
-      baseContext = {...baseContext, on:null};
-    }
-    // console.info('mergeMetric', metric, context, baseContext)
     if (Object.keys(metric).some(k=> clearsKey.includes(k))){
         return {...baseContext, ...metric}
     } else {
