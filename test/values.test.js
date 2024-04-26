e247aa507449c65db7eac82f02f4e5c502243832
rcowin <richard.cowin@gmail.com>
2024-04-26 13:44:05 -0500

    reformatted

--- test/values.test.js
+++ test/values.test.js
@@ -1,8 +1,8 @@
-import {convertValue, getValue } from "../src/values";
+import { convertValue, getValue } from "../src/values";
 import { resetTracking } from "../src/track";
 
 window.evolv = {
-  metrics: {}
+  metrics: {},
 };
 
 afterEach(() => {
@@ -13,36 +13,35 @@ afterEach(() => {
 });
 
 beforeEach(() => {
-    resetTracking();
+  resetTracking();
 });
 
-
-function getConvertedValue(metric, target){
-    let value = getValue(metric, target)
-    if (!value) return undefined;
-    return convertValue(value, metric.type);
+function getConvertedValue(metric, target) {
+  let value = getValue(metric, target);
+  if (!value) return undefined;
+  return convertValue(value, metric.type);
 }
 
 // source retrievals
 // other values tests are split into different files
 
-test('explicit string value', () => {
-    let metric = {value: 'test'};
-    expect(getConvertedValue(metric)).toBe('test');
+test("explicit string value", () => {
+  let metric = { value: "test" };
+  expect(getConvertedValue(metric)).toBe("test");
 });
 
-test('explicit number value', () => {
-    let metric = {value: 5, type: 'number'};
-    expect(getConvertedValue(metric)).toBe(5);
+test("explicit number value", () => {
+  let metric = { value: 5, type: "number" };
+  expect(getConvertedValue(metric)).toBe(5);
 });
 
-test('explicit number value', () => {
-    let metric = {value: true, type: 'boolean'};
-    expect(getConvertedValue(metric)).toBe(true);
+test("explicit number value", () => {
+  let metric = { value: true, type: "boolean" };
+  expect(getConvertedValue(metric)).toBe(true);
 });
 
-test('extensions supported', () => {
-  let metric = {source: "extension", key: "distribution", type: 'number'};
+test("extensions supported", () => {
+  let metric = { source: "extension", key: "distribution", type: "number" };
   expect(getConvertedValue(metric)).toBeGreaterThanOrEqual(0);
   expect(getConvertedValue(metric)).toBeLessThan(100);
 });
