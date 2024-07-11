5d64d05d3f8468c0465dd6fe74b54fe6f50e7a85
rcowin <richard.cowin@gmail.com>
2024-07-11 10:44:11 -0500

    forgot new file to provide and adapter for cookies

---
+++ src/cookieStore.js
@@ -0,0 +1,30 @@
+
+function getCookieDomain(){
+    const parsedDomain = location.host.match('([a-zA-Z0-9]+\.[a-zA-Z0-9]+)$'); 
+    return `.${parsedDomain[0]}`;
+}
+
+export function cookieStore(domain, durationInMinutes) {
+    if (!domain){
+        domain = getCookieDomain();
+    }
+    if (typeof durationInMinutes !== "number") {
+        durationInMinutes = 60*14;
+    }
+
+    return {
+        getItem(name){
+            var foundCookie = document.cookie.split(';')
+                .find(item =>  item.trim().split('=')[0] === name);
+
+            if (!foundCookie) return null;
+    
+            return decodeURIComponent(foundCookie.split('=')[1]);
+        },
+        setItem(name, value){
+            const val = encodeURIComponent(value);
+            const age = durationInMinutes*60; //age is session - todo: add option
+            document.cookie = `${name}=${val}; max-age="${age}"; path=/; domain=${domain}`;
+        }
+    }
+}
